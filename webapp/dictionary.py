f99da5b7d52b399a886ce54ed78a8c498eedf5f1
keple <keplerde@gmail.com>
2023-04-07 18:53:24 +0200

    get the definition in the Output-box

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,20 +1,31 @@
 import justpy as jp
+import definition
+
 class Dictionary:
     path = "/dictionary"
 
     @classmethod
-    def serve(cls, serve):
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-        jp.Div(a=div, text="This is the About page!", classes="text-4xl m-2")
+        jp.Div(a=div, text="Instant English Dictionary!", classes="text-4xl m-2")
         jp.Div(a=div, text="""
-                 Instant English Dictionary
-                """, classes="text-lg")   # it belongs to div
-        jp.Input(a=div, placeholder="Type in a word here...",
-                 classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 focus:bg-white focus:outline-none focus:border-purple-500 "
-                         "py-2 px-4 ")
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+                 Get the definition
+                """, classes="text-lg")  # it belongs to div
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 focus:bg-white focus:outline-none focus:border-purple-500 "
+                                     "py-2 px-4 ")
 
-        print(cls, serve)
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+
+        jp.Button(a=input_div, text="Get Definition", classes="border-2 text-gray-500", click=cls.get_definition,
+                  outputdiv=output_div, inputbox=input_box)
+
+        print(cls, req)
         return wp
 
+    @staticmethod
+    def get_definition(widget, msg):
+        defined = definition.Definition(widget.inputbox.value).get()
+        widget.outputdiv.text = " ".join(defined)  #separated by space
