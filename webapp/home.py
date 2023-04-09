086eb49375c5dbfabcf05cf755f3b9914f7cf0a1
keple <keplerde@gmail.com>
2023-04-09 11:50:27 +0200

    i intended to make the layout MenuBar

--- webapp/home.py
+++ webapp/home.py
@@ -3,11 +3,20 @@ import justpy as jp
 
 class Home:
     path = "/"
-
-
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left",
+                        bordered=True)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu", click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+        container = jp.QPageContainer(a=layout)
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="This is the About page!", classes="text-4xl m-2")
         jp.Div(a=div, text="""
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
@@ -22,6 +31,12 @@ class Home:
             """, classes="text-lg")
         return wp
 
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
 
 #jp.Route(Home.path, Home.serve)  # to refer we can access class
 #jp.justpy(port=8001)
