660b439f381e02eebaa196708a73d8cb3aa9127a
keple <keplerde@gmail.com>
2023-04-09 12:54:38 +0200

    default layout class for home, page, dictionary

--- webapp/home.py
+++ webapp/home.py
@@ -1,51 +1,40 @@
 import justpy as jp
-
+import layout
 
 class Home:
     path = "/"
+
     @classmethod
     def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
 
-        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
-        header = jp.QHeader(a=layout)
-        toolbar = jp.QToolbar(a=header)
+        lay = layout.DefaultLayout(a=wp)
 
-        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left",
-                        bordered=True)
-        scroller = jp.QScrollArea(a=drawer, classes="fit")
-        qlist = jp.QList(a=scroller)
-        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
-        jp.A(a=qlist, text="Home", href="/home", classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="Dictionary", href="/dictionary",classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="About", href="/about", classes=a_classes)
-        jp.Br(a=qlist)
-        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu", click=cls.move_drawer, drawer=drawer)
-        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
-        container = jp.QPageContainer(a=layout)
-        div = jp.Div(a=container, classes="bg-gray-200 h-screen")
-        jp.Div(a=div, text="This is the About page!", classes="text-4xl m-2")
+        container = jp.QPageContainer(a=lay)
+
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen p-2")
+        jp.Div(a=div, text="This is the Home page!", classes="text-4xl m-2")
         jp.Div(a=div, text="""
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-                     Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, 
-                     adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                     Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, 
-                     nisi ut aliquid ex ea commodi consequatur? 
-                     Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
-                     vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-            """, classes="text-lg")
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+                 Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, 
+                 adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+                 Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, 
+                 nisi ut aliquid ex ea commodi consequatur? 
+                 Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
+                 vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        """, classes="text-lg")
+
         return wp
 
-    @staticmethod
-    def move_drawer(widget, msg):
-        if widget.drawer.value:
-            widget.drawer.value = False
-        else:
-            widget.drawer.value = True
+
+    # @staticmethod
+    # def move_drawer(widget, msg):
+    #     if widget.drawer.value:
+    #         widget.drawer.value = False
+    #     else:
+    #         widget.drawer.value = True
 
 #jp.Route(Home.path, Home.serve)  # to refer we can access class
 #jp.justpy(port=8001)
