ff00273d1f099d2982f3de5164e325b3e12bb7b9
keple <keplerde@gmail.com>
2023-04-09 13:11:51 +0200

    imports through iterating

--- webapp/main.py
+++ webapp/main.py
@@ -4,8 +4,15 @@ from home import Home
 from about import About
 from dictionary import Dictionary
 
-jp.Route(Home.path, Home.serve)
-jp.Route(About.path, About.serve)
-jp.Route(Dictionary.path, Dictionary.serve)  # to refer we can access class
+imports = list(globals().values())     # globals() это встроенная функция, которая возвращает словарь, представляющий текущую глобальную таблицу символов.
+                                       # list of availiable names through iterating
+for obj in imports.items():
+    if hasattr(obj, 'path'):
+        jp.Route(obj.path, obj.serve)
+
+
+# jp.Route(Home.path, Home.serve)
+# jp.Route(About.path, About.serve)
+# jp.Route(Dictionary.path, Dictionary.serve)  # to refer we can access class
 
 jp.justpy(port=8001)
